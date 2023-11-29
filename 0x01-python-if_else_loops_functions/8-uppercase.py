0f260222ccbd580a921eb1eea0a5ac972c8ed4a9
Ini <arthurshur@gmail.com>
2023-11-29 09:19:42 -0800

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    j = 1
-    ln = len(str)
+    j = 0
+    if len(str) == 0:
+        str = " "
+    ln = len(str) - 1
     for i in str:
         if ord(i) > 65 and ord(i) > 90:
             print('{}'.format(chr(ord(i)-32)), end="" if j < ln else "\n")
