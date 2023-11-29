0d7d8a4f06f5f7137da5f66592263afacc90b1b9
Ini <arthurshur@gmail.com>
2023-11-28 21:04:46 -0800

    skip q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,4 @@ for char in range(97, 123):
     if chr(char) == 'e' or chr(char) == 'q':
         continue;
     else:
-        print(chr(char), end='')
+        print('{}'.format(chr(char)), end='')
