cb144dafab7841b0d585b2a1321a76b549d0f4a6
Ini <arthurshur@gmail.com>
2023-11-29 02:59:52 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(00, 99):
-        print("{},".format(str(i).zfill(2)), end=" ")
+    print("{},".format(str(i).zfill(2)), end=" ")
 
-print("{}\n".format(99))
+print("{}".format(99), end="\n")
