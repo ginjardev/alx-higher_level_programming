8f094038b1f2cc12e1c8b60badc3a5a0b3afb6d8
Ini <arthurshur@gmail.com>
2023-12-01 16:06:54 -0800

    my calculator_1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    av = sys.argv
+    if len(sys.argv) != 4:
+        print(len(sys.argv))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif av[2] != '+' or av[2] != '-' or av[2] != '*' or av[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    elif av[2] == '+':
+        print("yes")
+        add(int(av[1]), int(av[1]))
+    elif av[2] == '-':
+        sub(int(av[1]), int(av[1]))
+    elif av[2] == '*':
+        mul(int(av[1]), int(av[1]))
+    elif av[2] == '/':
+        div(int(av[1]), int(av[1]))
+    sys.exit(0)
