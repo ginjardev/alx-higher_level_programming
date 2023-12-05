24c64190dfe81a8d5a641995e7a6e92f177ec254
Ini <arthurshur@gmail.com>
2023-12-04 17:29:27 -0800

    replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if idx == i:
+                my_list[i] = element
+                return my_list
+
+
+if __name__ == "__main__":
+    replace_in_list(my_list, idx, element)
