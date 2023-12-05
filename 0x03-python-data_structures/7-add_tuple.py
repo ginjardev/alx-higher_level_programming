f024d23e31017c4549f42df8306dd7ce88b432b1
Ini <arthurshur@gmail.com>
2023-12-05 08:11:29 -0800

    tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0,0)
     tuple_b += (0,0)
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_c =  (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_c
 
 
 if __name__ == "__main__":
