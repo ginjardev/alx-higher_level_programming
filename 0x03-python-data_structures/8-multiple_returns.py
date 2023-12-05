743685721233f688b78d80dd0575ae370a5e6686
Ini <arthurshur@gmail.com>
2023-12-05 09:00:31 -0800

    more returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        return
+    if len(sentence) == 0:
+        return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
 
