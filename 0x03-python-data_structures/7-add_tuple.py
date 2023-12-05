c1edf88067272c495bb2c580bbebe6b6b81e325a
Ini <arthurshur@gmail.com>
2023-12-05 08:06:19 -0800

    tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,12 +3,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a is None or tuple_b is None:
         return
-    elif len(tuple_a) == 2 and len(tuple_b) == 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) == 2 and len(tuple_b) == 1:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif len(tuple_a) == 2 and len(tuple_b) == 0:
-        return tuple_a
+    tuple_a += (0,0)
+    tuple_b += (0,0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
 
 if __name__ == "__main__":
