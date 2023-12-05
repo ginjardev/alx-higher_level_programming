248888f370d4aec5a7b46c6ef5f9cb44557caa43
Ini <arthurshur@gmail.com>
2023-12-05 15:09:09 -0800

    only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-   if my_list is None:
-       return
-   new_list = []
+    if my_list is None:
+        return
 
-   for i in my_list:
-       if i % 2 == 0:
-           new_list.append(True)
-       else:
-           new_list.append(False)
+    new_list = []
 
-   return new_list
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
 
 
 if __name__ == "__main__":
