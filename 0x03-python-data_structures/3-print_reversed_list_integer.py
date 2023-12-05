19dbd329230090f7aa454feedc1c4229de4f76de
Ini <arthurshur@gmail.com>
2023-12-05 03:10:54 -0800

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     for i in range((len(my_list) - 1), -1, -1):
-        print(my_list[i])
+        print('{:d}'.format(my_list[i]))
 
 
 if __name__ == "__name__":
