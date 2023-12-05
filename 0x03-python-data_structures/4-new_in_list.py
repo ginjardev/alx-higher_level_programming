b6caabf3747956d657fa0986ab988b32c05d257c
Ini <arthurshur@gmail.com>
2023-12-05 03:40:12 -0800

    replace in copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    elif idx < 0:
+        return my_list
+    elif idx >= len(myList):
+        return my_list
+    else:
+        new_list = []
+        for i in range(len(my_list)):
+            if idx != my_list[i]:
+                new_list[i] = my_list[i]
+            else:
+                new_list[i] = element
+    return new_list
+
+
+if __name__ == "__main__":
+    new_in_list(my_list, idx, element)
