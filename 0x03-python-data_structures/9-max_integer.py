6541973c24e64b96de8232f7b6df4a80ae1b0c32
Ini <arthurshur@gmail.com>
2023-12-05 09:20:35 -0800

    find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max = 0
     if len(my_list) == 0:
         return None
-    else:
-        for i in my_list:
-            if i > max:
-                max = i
+
+    max = my_list[0]
+    for i in my_list:
+        if i > max:
+            max = i
     return max
 
 
