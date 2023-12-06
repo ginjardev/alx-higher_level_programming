11ff00a75f6ca77a01072a2ecdfc83412402cc72
Ini <arthurshur@gmail.com>
2023-12-05 16:13:49 -0800

    calculate

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    av = sys.argv
-    if len(sys.argv) != 4:
-        print(len(sys.argv))
+def calculate(argc, argv):
+    if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    elif av[2] != '+' or av[2] != '-' or av[2] != '*' or av[2] != '/':
+        exit(1)
+
+    a = int(argv[1])
+    op = av[2]
+    b = int(argv[3])
+
+    if op == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    elif av[2] == '+':
-        print("yes")
-        add(int(av[1]), int(av[1]))
-    elif av[2] == '-':
-        sub(int(av[1]), int(av[1]))
-    elif av[2] == '*':
-        mul(int(av[1]), int(av[1]))
-    elif av[2] == '/':
-        div(int(av[1]), int(av[1]))
-    sys.exit(0)
+        exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(sys.argv)
+
+    my_calculator(argc, argv)
