527ddaca7843eb4b36dbf5ecf765889be6845eba
Ini <arthurshur@gmail.com>
2023-12-08 14:37:21 -0800

    squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,3 +7,7 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_matrix.append(list(map(lambda x: x**2, row)))
     return new_matrix
+
+
+if __name__ == "__main__":
+    square_matrix_simple(matrix=[])
