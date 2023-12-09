5a19530146e191937413531e05dfbe1c68b2ea88
Ini <arthurshur@gmail.com>
2023-12-09 03:54:47 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,13 +5,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    new_list = list(a_dictionary.items())
-    max_ = new_list[0][1]
-    key = " "
-    for i in range(len(new_list)):
-        if new_list[i][1] > max_:
-            max_ = new_list[i][1]
-            key = new_list[i][0]
+    key = max(a_dictionary, key=a_dictionary.get)
 
     return key
 
