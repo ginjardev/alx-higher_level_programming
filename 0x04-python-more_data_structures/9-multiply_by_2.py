f2534ac3ca3618127493a320cb19c6dae7df4d06
Ini <arthurshur@gmail.com>
2023-12-09 02:32:31 -0800

    mul by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.fromkeys(a_dictionary.keys())
     values = list(a_dictionary.values())
@@ -13,3 +12,7 @@ def multiply_by_2(a_dictionary):
             break
 
     return new_dict
+
+
+if __name__ == "__main__":
+    multiply_by_2(a_dictionary)
