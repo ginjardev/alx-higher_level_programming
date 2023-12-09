7012c95a95ef7fa7fe61d3b336fea7a315a47c1a
Ini <arthurshur@gmail.com>
2023-12-09 01:31:42 -0800

    simple delete

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    del a_dictionary[key]
+    return a_dictionary
+
+
+if __name__ == "__main__":
+    simple_delete(a_dictionary, key="")
