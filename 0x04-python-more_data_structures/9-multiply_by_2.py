1836666ec273e7aacbd70747cec54c474cd379b0
Ini <arthurshur@gmail.com>
2023-12-09 02:28:25 -0800

    multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.fromkeys(a_dictionary.keys())
+    values = list(a_dictionary.values())
+
+    i = 0
+    for key in new_dict.keys():
+        for j in range(len(values)):
+            new_dict[key] = values[j + i] * 2
+            i = i + 1
+            break
+
+    return new_dict
