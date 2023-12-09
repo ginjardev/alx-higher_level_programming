708dd03273e53c5db20cf25ede30859956828ed9
Ini <arthurshur@gmail.com>
2023-12-09 03:36:47 -0800

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    
+    new_list = list(a_dictionary.items())
+
+    max = new_list[0][1]
+    key = " "
+    for i in range(len(new_list)):
+        if new_list[i][1] > max:
+            max = new_list[i][1]
+            key = new_list[i][0]
+
+    return key
+
+
+if __name__ == "__main__":
+    best_score(a_dictionary)
