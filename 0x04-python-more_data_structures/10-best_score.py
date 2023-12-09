b9698d39b34ccaecd556ba6b87eec4b047792877
Ini <arthurshur@gmail.com>
2023-12-09 03:58:24 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,11 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary:
+        best_key = max(a_dictionary, key=a_dictionary.get)
+        return best_key
+    else:
         return None
-
-    key = max(a_dictionary, key=a_dictionary.get)
-
-    return key
 
 
 if __name__ == "__main__":
