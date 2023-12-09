d3a42c85ebdf742afdcab23c0bd663c33abfe221
Ini <arthurshur@gmail.com>
2023-12-09 07:10:15 -0800

    mul list map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], number=0):
-    result = list(map(lambda x: x * number, my_list))
-    return result
-
-
-if __name__ == "__name__":
-    multiply_list_map(my_list=[], number=0)
+    return list(map(lambda x: x * number, my_list))
