ff6db97d23526035f7a90ebfbd9d1d204a7a95c5
Ini <arthurshur@gmail.com>
2023-12-08 17:05:25 -0800

    add unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,18 +2,9 @@
 
 
 def uniq_add(my_list=[]):
-    check = 0
     sum = 0
-    for i in my_list:
-        for j in my_list:
-            if i == j:
-                check = check + 1
-        if check == 1:
-            sum = sum + i
-        check = 0
-
-    return sum
-
+    for i in set(my_list):
+        sum = sum + i
 
 if __name__ == "__main__":
     uniq_add(my_list=[])
