d1f294a6c29ddb3f6b216be9f652e43af8adb3c7
Ini <arthurshur@gmail.com>
2023-12-18 12:47:17 -0800

    print ints

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 counter += 1
-        except ValueError:
-            continue
+            else:
+                continue
+        except IndexError:
+            pass
     print()
     return counter
