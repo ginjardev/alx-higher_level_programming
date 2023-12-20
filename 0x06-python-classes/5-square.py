9ea6594dff69db7c007ec91cff5e569bae22c1a0
Ini <arthurshur@gmail.com>
2023-12-19 18:39:57 -0800

    print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -21,10 +21,15 @@ class Square:
 
     @property
     def size(self):
+        """getter method, gets size attribute
+        Returns:
+            int: size attribute
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """setter method, sets size attribute """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -39,3 +44,13 @@ class Square:
             int: the area of square
         """
         return self.__size**2
+
+    def my_print(self):
+        """ prints square based on size attribute"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
