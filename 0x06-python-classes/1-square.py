efad63ce8cfd0ea0d55716a3101d54f05172f8a9
Ini <arthurshur@gmail.com>
2023-12-19 17:01:36 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This model defines a class Sqaure with a `size` attribute """
+
+
+class Square:
+    """A simple python class Square"""
+
+    def __init__(self, size):
+        """The __init__ method initializes instance object
+        Args:
+            size (int): size parameter for Square class
+        """
+        self.__size = size
