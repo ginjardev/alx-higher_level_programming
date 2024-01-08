7173df35f15ce9d80d0b9c28d8701f292212ecaf
Ini <arthurshur@gmail.com>
2024-01-08 05:22:00 -0800

    static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -105,7 +105,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-    
+
         if rect_1.area == rect_2.area:
             return rect_1
         elif rect_1.area > rect_2.area:
