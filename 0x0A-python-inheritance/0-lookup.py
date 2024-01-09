23d481750c56f5b4677b58b4ad6c095a748b01e7
Ini <arthurshur@gmail.com>
2024-01-08 17:58:48 -0800

    return attr

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Lists available attributes and methods of an object"""
+"""Lookup Module - Lists available attributes and methods of an object"""
 
 def lookup(obj):
     """List attributes of an object
     Args:
-        obj: object instance of a class
+        obj (object): object instance of a class
     Returns: list of attributes
     """
     return dir(obj)
