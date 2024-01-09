6568dd996176d2965a882fca3ddc50fb90eb8ce9
Ini <arthurshur@gmail.com>
2024-01-08 18:50:54 -0800

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Checks if obj is instance a class """
+
+
+def is_same_class(obj, a_class):
+    """Checks if an object is an instance of a class
+
+    Args:
+      obj (object): Object to check.
+      a_class: Class to check.
+
+    Return:
+      True if the object is instance of the class otherwise False
+    """
+    return isinstance(obj, a_class)
