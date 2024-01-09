53ae4aca41dce4231afe597ce3481a49dfd26d6c
Ini <arthurshur@gmail.com>
2024-01-08 18:35:23 -0800

    is instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""checks if object is instance of a class"""
+
+def is_same_class(obj, a_class):
+    """checks if object is instance of class
+
+    Args:
+        obj (object): object of Python class 
+        a_class (class): custom Python class
+
+    Returns:
+        True if obj is instance otherwise False
+    """
+    return isinstance(obj, a_class)
