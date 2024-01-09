d89a3387c3708b0f2121f319ae7ff26110cfac05
Ini <arthurshur@gmail.com>
2024-01-08 19:04:37 -0800

    inherited

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Checks if obj is instance of a class inherited (directly or indirectly) """
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is instance of a class (directly or indirectly)
+
+    Args:
+      obj (object): Object to check.
+      a_class: Class to check.
+
+    Return:
+      True if the object is instance of the class False otherwise
+    """
+    if type(obj) == a_class:
+        return True
+    elif isinstance(obj, a_class):
+        return True
+    else:
+        return False
