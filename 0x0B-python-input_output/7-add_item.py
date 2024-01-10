621ef48e4b90abaf7e35acf20e29940dffb69401
Ini <arthurshur@gmail.com>
2024-01-10 12:37:28 -0800

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list, and then save them to a file"""
+"""This module adds arguments to Python list, and save them to a file"""
 import json
 import sys
 
