432c5348b0507710c6739cf53c26f4b3a3b8b477
Ini <arthurshur@gmail.com>
2024-01-10 03:23:42 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module with function to read file to STDOUT"""
+
+
+def read_file(filename=""):
+    """Function reads file
+    Args:
+        filename (object): file object
+    """
+    with open(filename, mode='r', encoding='utf-8') as fp:
+        print(fp.read())
