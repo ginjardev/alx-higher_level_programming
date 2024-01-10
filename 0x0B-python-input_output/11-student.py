2f260ad54abc8a9d3fd2becc293f259012104531
Ini <arthurshur@gmail.com>
2024-01-10 15:14:22 -0800

    student json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A module with a Student class"""
+
+
+class Student:
+    """A simple Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student class instance
+
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student object
+
+        Args:
+            attrs: object attributes
+        """
+        if attrs is None:
+            return self.__dict__
+        return {atr: getattr(self, atr) for atr in attrs if hasattr(self, atr)}
+
+    def reload_from_json(self, json):
+        """Reload student attributes from a json file.
+
+        Args:
+          json: JSON dict.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
