b9e596d4d7a6e9025ef441a9fd57f88bcb5596a6
Ini <arthurshur@gmail.com>
2024-01-11 06:37:35 -0800

    copy list

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module makes copies of a list"""
+
+
+def copy_list(l):
+    """Function copies a list
+
+    Args:
+        l (list): list parameter
+    """
+    copy = []
+    copy = l[:]
+    return copy
