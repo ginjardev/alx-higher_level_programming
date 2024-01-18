631721d3ceaae304248141e8ac834d7da4db717e
Ini <arthurshur@gmail.com>
2024-01-18 09:00:08 -0800

    str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,12 +109,11 @@ class Rectangle(Base):
         """Function prints Rectangle to STDOUT"""
         for i in range(self.__height):
             for j in range(self.__width):
-                print("#", end='')
+                print("#", end="")
             print()
 
     def __str__(self):
-        """ Return a string represntation of the object Rectangle """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                        self.x, self.y,
-                                                        self.width,
-                                                        self.height)
+        """Return a string represntation of the object Rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
