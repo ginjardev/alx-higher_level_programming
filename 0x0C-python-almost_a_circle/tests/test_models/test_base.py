7ce7edd088938495543dedc0c9b0263df9959c03
Ini <arthurshur@gmail.com>
2024-01-17 17:47:38 -0800

    change

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,9 +7,13 @@ import unittest
 class TestBase(unittest.TestCase):
     def setUp(self):
         self.base = Base()
+    
+    def tearDown(self):
+        del self.base
 
     def test_base(self):
         self.assertTrue(self.base)
 
     def test_object_id(self):
+        print(self.base.id)
         self.assertEqual(self.base.id, 1)
