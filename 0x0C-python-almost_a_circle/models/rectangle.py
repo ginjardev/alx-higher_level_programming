e95ddb208817d871aeffdb70f7bf6d8e7f522efa
Ini <arthurshur@gmail.com>
2024-01-18 03:09:31 -0800

    validate input

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,27 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """initializes Rectangle object with width, height, x, y and id"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        elif type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        elif type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        elif type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__width = width
+            self.__height = height
+            self.__x = x
+            self.__y = y
 
     @property
     def width(self):
@@ -26,6 +43,10 @@ class Rectangle(Base):
         Args:
             value (int): integer parameter
         """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,6 +60,10 @@ class Rectangle(Base):
         Args:
             value (int): int parameter
         """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -52,6 +77,10 @@ class Rectangle(Base):
         Args:
             value (int): int parameter
         """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -66,4 +95,8 @@ class Rectangle(Base):
         Args:
             value (int): int parameter
         """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
