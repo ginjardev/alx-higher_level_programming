3e05c6f556a435db96865eec117209333db29281
Ini <arthurshur@gmail.com>
2024-02-16 02:40:02 -0800

    full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-USE hbtn_0c_0; 
-SELECT * 
-FROM information_schema.columns 
-WHERE table_name = 'first_table';
+SHOW CREATE TABLE first_table;
