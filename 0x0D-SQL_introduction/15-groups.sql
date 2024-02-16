8e715202ce9b18f5d153edd18e7d783888bb0b6e
Ini <arthurshur@gmail.com>
2024-02-16 04:00:30 -0800

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- lists the number of records with the same score in the table second_table
 SELECT score, count(score) as number
 FROM second_table
-GROUP By score;
+GROUP By score
+ORDER BY score DESC; 
 
