dd0b604bf40cecc7887be61f684ab48712461051
Ini <arthurshur@gmail.com>
2024-02-18 05:48:04 -0800

    california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,8 @@
--- ists all the cities of California that can be found in the database hbtn_0d_usa
-USE hbtn_0d_usa;
-
-SELECT c.id, c.name FROM cities c
-WHERE c.state_id IN (
-	SELECT s.id
-	FROM states s
-	WHERE s.name = "California"
-)
-ORDER BY c.id;
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
