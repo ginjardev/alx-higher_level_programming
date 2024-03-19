0bf25cc523fe34ce0f90147a6f1c050db24a5697
Ini <arthurshur@gmail.com>
2024-03-19 15:19:39 -0700

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'JavaScript is amazing';
-
-console.log(`${first} \n${second} \n${third}`);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
