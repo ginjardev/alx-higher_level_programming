36be7d0bf05b11212b899dd63b9d73b85ae00b7c
Ini <arthurshur@gmail.com>
2024-03-30 03:12:48 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+for (let i = 0; i < 3; i++) {
+  switch (i) {
+    case 0:
+      console.log('C is fun');
+      break;
+    case 1:
+      console.log('Python is cool');
+      break;
+    default:
+      console.log('JavaScript is amazing');
+  }
+}
