4ba09a23a218b170f27d384f1d116b607e495412
ginjardev <arthurshur@gmail.com>
2024-04-03 20:07:29 -0700

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
+
 const list = require('./100-data').list;
 
-const newList = list.map(function (num, index) {
-	return num * index;
-});
+const newList = list.map((num, index) => num * index);
 
 console.log(list);
 console.log(newList);
