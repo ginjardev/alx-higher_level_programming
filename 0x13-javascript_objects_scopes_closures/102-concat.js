e737345bc8776207f3fdb4b0c540eac18ddf72c8
ginjardev <arthurshur@gmail.com>
2024-04-03 20:15:00 -0700

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const fs = require('fs');
+const argv = process.argv;
+
+const file1 = fs.readFileSync(argv[2], 'utf-8').toString();
+const file2 = fs.readFileSync(argv[3], 'utf-8').toString();
+fs.writeFileSync(argv[4], file1 + file2);
