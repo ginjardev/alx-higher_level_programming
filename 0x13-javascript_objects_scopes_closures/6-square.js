67bc5ec7368c18e8d1fa190f22cbb91008d70bf2
ginjardev <arthurshur@gmail.com>
2024-04-03 17:54:52 -0700

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let join = '';
+      for (let j = 0; j < this.width; j++) {
+        join += c;
+      }
+      console.log(join);
+    }
+  }
+}
+
+module.exports = Square;
