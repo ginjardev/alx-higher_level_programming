3f1fe6d2fe1b59a6bb688a115b6f9d71b9237539
ginjardev <arthurshur@gmail.com>
2024-04-03 20:11:59 -0700

    task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
+
 const newDict = {};
 
-Object.keys(dict).map(function (key, index) {
-	if (newDict[dict[key]] === undefined) {
-		newDict[dict[key]] = [];
-	}
-	newDict[dict[key]].push(key);
-});
+for (const [key, value] of Object.entries(dict)) {
+  newDict[value] ? newDict[value].push(key) : newDict[value] = [key];
+}
 
 console.log(newDict);
