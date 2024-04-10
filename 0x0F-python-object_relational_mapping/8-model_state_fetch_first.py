97e06bcad0b917f1eb0bf02f2e9d23094e487a9e
ginjardev <arthurshur@gmail.com>
2024-04-10 05:30:13 -0700

    task 9

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
+"""script that prints the first State
+object from the database hbtn_0e_6_usa"""
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
@@ -19,3 +20,4 @@ if __name__ == "__main__":
         print(f"{state.id}: {state.name}")
     else:
         print("Nothing")
+    session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
+"""lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
@@ -14,6 +15,8 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id)
     for state in states:
         print(f"{state.id}: {state.name}")
+    session.close()
